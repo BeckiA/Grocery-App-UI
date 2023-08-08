0c2db995821e6039687ae6b7cfe604e5cf1ef2f5
becki <berekeetabera@gmail.com>
2023-08-08 22:53:18 +0300

    Created the details page with interactive UI's

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/helpers/utils.dart';
 import 'package:grocery_app/models/subcategory.dart';
 import 'package:grocery_app/screens/category_list_screen.dart';
 import 'package:grocery_app/screens/details_screen.dart';
@@ -9,7 +10,8 @@ void main() {
   runApp(MaterialApp(
     theme: ThemeData(fontFamily: "Raleway"),
     debugShowCheckedModeBanner: false,
-    home: SplashScreen(duration: 3, goToPage: WelcomeScreen()),
-    // home: DetailsScreen(subCategory: subCategory),
+    // home: SplashScreen(duration: 3, goToPage: WelcomeScreen()),
+    home: DetailsScreen(
+        subCategory: Utils.getMockedCategories()[0].subCategories[0]),
   ));
 }
--- lib/models/subcategory.dart
+++ lib/models/subcategory.dart
@@ -1,13 +1,17 @@
 import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
 import 'package:grocery_app/models/category.dart';
+import 'package:grocery_app/models/categorypart.dart';
 
 class SubCategory extends Categories {
+  List<CategoryPart> parts;
   SubCategory({
     required String name,
     required String icon,
     required Color color,
     required String imgName,
+    required this.parts,
   }) : super(
             name: name,
             icon: icon,
